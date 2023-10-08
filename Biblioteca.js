c93d0a1a8005341971866cdb689f55595ddff469
AlejandroMancilla <137446227+AlejandroMancilla@users.noreply.github.com>
2023-10-07 21:16:11 -0500

    Faltan Estilos

--- Biblioteca.js
+++ Biblioteca.js
@@ -18,7 +18,9 @@ const BtnOrdReverse = document.getElementById('OrdInverter');
 const ListSide = document.getElementById('sidebar');
 const CloseList = document.getElementById('CloseSideBar');
 const NameList = document.getElementById('NameList');
+const TextList = document.getElementById('TextList');
 const List = document.getElementById('Lista');
+const Tabla = document.getElementById('TableBooks');
 
 var Books = [];
 var Order = 0;
@@ -50,116 +52,15 @@ BtnAdd.addEventListener('click', function(){
 BtnDelete.addEventListener('click', function(){
   ListSide.classList.toggle('active');
   NameList.innerHTML = 'Delete Books';
-  const Tabla = document.createElement('table');
-  const TblHead = document.createElement('thead');
-  const TblBody = document.createElement('tbody');
-  var Encabezado = document.createElement('tr');
-  var Enc1 = document.createElement('th');
-  Enc1.style.width = '60%';
-  var TxtEnc1 = document.createTextNode('BOOK');
-  var Enc2 = document.createElement('th');
-  Enc2.style.width = '40%';
-  var TxtEnc2 = document.createTextNode('AUTHOR');
-  Enc1.appendChild(TxtEnc1);
-  Enc2.appendChild(TxtEnc2);
-  Encabezado.appendChild(Enc1);
-  Encabezado.appendChild(Enc2);
-  TblHead.appendChild(Encabezado);
-
-  var Cont = 0
-  OrderBy(1);        
-  Books.forEach(function(x) {
-      var Fila = document.createElement('tr');
-      Fila.id = Cont;
-      var CeldaName = document.createElement('td');
-      var TxtCelda = document.createTextNode(x.Title);
-      Fila.addEventListener('click', function(){
-        console.log(Fila.id);
-        DeleteBook(Fila.id);
-      });
-      var CeldaAuthor = document.createElement('td');
-      var TxtAuthor = document.createTextNode(x.Author);
-      CeldaName.appendChild(TxtCelda);
-      Fila.appendChild(CeldaName);
-      CeldaAuthor.appendChild(TxtAuthor);
-      Fila.appendChild(CeldaAuthor);
-      TblBody.appendChild(Fila);
-      Cont++;
-  });
-  Tabla.appendChild(TblHead);
-  Tabla.appendChild(TblBody);
-  Tabla.setAttribute('border', '2');
-  List.innerHTML = '';
-  List.appendChild(Tabla);
+  TextList.innerHTML = 'Click on the book you want to delete'
+  UpdateTableDelete();
 });
 
 BtnReserve.addEventListener('click', function(){
   ListSide.classList.toggle('active');
   NameList.innerHTML = 'Reserve Books';
-  const Tabla = document.createElement('table');
-  const TblHead = document.createElement('thead');
-  const TblBody = document.createElement('tbody');
-  var Encabezado = document.createElement('tr');
-  var Enc1 = document.createElement('th');
-  Enc1.style.width = '60%';
-  var TxtEnc1 = document.createTextNode('BOOK');
-  var Enc2 = document.createElement('th');
-  Enc2.style.width = '20%';
-  var TxtEnc2 = document.createTextNode('STATUS');
-  var Enc3 = document.createElement('th');
-  Enc3.style.width = '20%';
-  var TxtEnc3 = document.createTextNode('LOAN TO');
-  Enc1.appendChild(TxtEnc1);
-  Enc2.appendChild(TxtEnc2);
-  Enc3.appendChild(TxtEnc3);
-  Encabezado.appendChild(Enc1);
-  Encabezado.appendChild(Enc2);
-  Encabezado.appendChild(Enc3);
-  TblHead.appendChild(Encabezado);
-
-  var Cont = 0
-  OrderBy(4);
-  Books.reverse();        
-  Books.forEach(function(x) {
-      var Fila = document.createElement('tr');
-      Fila.id = Cont;
-      var CeldaName = document.createElement('td');
-      var TxtCelda = document.createTextNode(x.Title);
-      Fila.addEventListener('click', function(){
-        ReserveBook(Fila.id);
-      });
-      var CeldaStatus = document.createElement('td');
-      CeldaStatus.setAttribute('style', 'text-align : center');
-      if(x.Available == 'True') {
-        var TxtStatus = document.createTextNode('Available');
-        Fila.classList = 'RowGreen';
-      }else {
-        var TxtStatus = document.createTextNode('Loaned');
-        Fila.classList = 'RowRed';
-      }
-      var CeldaTaker = document.createElement('td');
-      CeldaTaker.setAttribute('style', 'text-align : center');
-      if(x.Loan == '') {
-        var TxtTaker = document.createTextNode('--');
-      }else {
-        var TxtTaker = document.createTextNode(x.Loan);
-      }
-
-
-      CeldaName.appendChild(TxtCelda);
-      Fila.appendChild(CeldaName);
-      CeldaStatus.appendChild(TxtStatus);
-      Fila.appendChild(CeldaStatus);
-      CeldaTaker.appendChild(TxtTaker);
-      Fila.appendChild(CeldaTaker);
-      TblBody.appendChild(Fila);
-      Cont++;
-  });
-  Tabla.appendChild(TblHead);
-  Tabla.appendChild(TblBody);
-  Tabla.setAttribute('border', '2');
-  List.innerHTML = '';
-  List.appendChild(Tabla);
+  TextList.innerHTML = 'Click on the book you want to Reserve/Unreserve' 
+  UpdateTableReserve();
 });
 
 BtnOrdName.addEventListener('click', function(){
@@ -223,11 +124,6 @@ function UpdateBooks(){
     const NewCard = document.createElement('div');
     NewCard.classList = "card";
     NewCard.id = x.Title;
-    if(x.Available=='True'){
-      NewCard.setAttribute("style", "background-color:rgba(204,255,204,0.7);");
-    }else{
-      NewCard.setAttribute("style", "background-color:rgba(255,204,204,0.7);");
-    }
     const Portada = document.createElement('img');
     Portada.classList = "portada";
     Portada.src = x.Cover;
@@ -237,10 +133,19 @@ function UpdateBooks(){
     AuthorText.innerHTML = x.Author;
     const YearText = document.createElement('h3');
     YearText.innerHTML = x.Year;
+    const LoanText = document.createElement('h3');
+    if(x.Available=='True'){
+      NewCard.setAttribute("style", "background-color:rgba(204,255,204,0.7);");
+    }else{
+      NewCard.setAttribute("style", "background-color:rgba(255,204,204,0.7);");
+      TitleText.setAttribute("style", "text-decoration: line-through");
+      LoanText.innerHTML = "Loan to: " + x.Loan;
+    }
     NewCard.appendChild(Portada);
     NewCard.appendChild(TitleText);
     NewCard.appendChild(AuthorText);
     NewCard.appendChild(YearText);
+    NewCard.appendChild(LoanText);
     ShowBooks.appendChild(NewCard);
   });
 };
@@ -314,4 +219,160 @@ function DeleteBook(N) {
   localStorage.setItem('Libros', JSON.stringify(Books));
 };
 
-function ReserveBook(N){}
+function ReserveBook(N){
+  console.log(Books[N].Available);
+  if(Books[N].Available == 'True'){
+    Books[N].Available = 'False';
+  }else{
+    Books[N].Available = 'True';
+    Books[N].Loan = '';
+  }
+  UpdateBooks();
+  UpdateTableReserve();
+};
+
+function UpdateTableDelete(){
+  Tabla.innerHTML = '';
+  const TblHead = document.createElement('thead');
+  const TblBody = document.createElement('tbody');
+  var Encabezado = document.createElement('tr');
+  var Enc1 = document.createElement('th');
+  Enc1.style.width = '60%';
+  var TxtEnc1 = document.createTextNode('BOOK');
+  var Enc2 = document.createElement('th');
+  Enc2.style.width = '40%';
+  var TxtEnc2 = document.createTextNode('AUTHOR');
+  Enc1.appendChild(TxtEnc1);
+  Enc2.appendChild(TxtEnc2);
+  Encabezado.appendChild(Enc1);
+  Encabezado.appendChild(Enc2);
+  TblHead.appendChild(Encabezado);
+  var RowSearch = document.createElement('tr');
+  var Search = document.createElement('td');
+  Search.colSpan = 3;
+  var InputSearch = document.createElement('input');
+  InputSearch.type = 'text';
+  InputSearch.placeholder = 'Write something to filter';
+  InputSearch.setAttribute("style", "width: 400px");
+  InputSearch.addEventListener('keyup', function(){
+    texto = InputSearch.value.toLowerCase();
+      var r=0;
+      while(row = TblBody.rows[r++])
+      {
+        if ( row.innerText.toLowerCase().indexOf(texto) !== -1 )
+          row.style.display = null;
+        else
+          row.style.display = 'none';
+      }
+    });
+  Search.appendChild(InputSearch);
+  RowSearch.appendChild(Search);
+  TblHead.appendChild(RowSearch);
+
+  var Cont = 0
+  OrderBy(1);        
+  Books.forEach(function(x) {
+      var Fila = document.createElement('tr');
+      Fila.classList = 'RowWhite';
+      Fila.id = Cont;
+      var CeldaName = document.createElement('td');
+      var TxtCelda = document.createTextNode(x.Title);
+      Fila.addEventListener('click', function(){
+        console.log(Fila.id);
+        DeleteBook(Fila.id);
+      });
+      var CeldaAuthor = document.createElement('td');
+      var TxtAuthor = document.createTextNode(x.Author);
+      CeldaName.appendChild(TxtCelda);
+      Fila.appendChild(CeldaName);
+      CeldaAuthor.appendChild(TxtAuthor);
+      Fila.appendChild(CeldaAuthor);
+      TblBody.appendChild(Fila);
+      Cont++;
+  });
+  Tabla.appendChild(TblHead);
+  Tabla.appendChild(TblBody);
+};
+
+function UpdateTableReserve(){
+  Tabla.innerHTML = '';
+  const TblHead = document.createElement('thead');
+  const TblBody = document.createElement('tbody');
+  var Encabezado = document.createElement('tr');
+  var Enc1 = document.createElement('th');
+  Enc1.style.width = '60%';
+  var TxtEnc1 = document.createTextNode('BOOK');
+  var Enc2 = document.createElement('th');
+  Enc2.style.width = '20%';
+  var TxtEnc2 = document.createTextNode('STATUS');
+  var Enc3 = document.createElement('th');
+  Enc3.style.width = '20%';
+  var TxtEnc3 = document.createTextNode('LOAN TO');
+  Enc1.appendChild(TxtEnc1);
+  Enc2.appendChild(TxtEnc2);
+  Enc3.appendChild(TxtEnc3);
+  Encabezado.appendChild(Enc1);
+  Encabezado.appendChild(Enc2);
+  Encabezado.appendChild(Enc3);
+  TblHead.appendChild(Encabezado);
+  var RowSearch = document.createElement('tr');
+  var Search = document.createElement('td');
+  Search.colSpan = 3;
+  var InputSearch = document.createElement('input');
+  InputSearch.type = 'text';
+  InputSearch.placeholder = 'Write something to filter';
+  InputSearch.setAttribute("style", "width: 400px");
+  InputSearch.addEventListener('keyup', function(){
+    texto = InputSearch.value.toLowerCase();
+      var r=0;
+      while(row = TblBody.rows[r++])
+      {
+        if ( row.innerText.toLowerCase().indexOf(texto) !== -1 )
+          row.style.display = null;
+        else
+          row.style.display = 'none';
+      }
+    });
+  Search.appendChild(InputSearch);
+  RowSearch.appendChild(Search);
+  TblHead.appendChild(RowSearch);
+
+  var Cont = 0
+  OrderBy(4);
+  Books.reverse();
+  Books.forEach(function(x) {
+      var Fila = document.createElement('tr');
+      Fila.id = Cont;
+      var CeldaName = document.createElement('td');
+      var TxtCelda = document.createTextNode(x.Title);
+      Fila.addEventListener('click', function(){
+        ReserveBook(Fila.id);
+      });
+      var CeldaStatus = document.createElement('td');
+      CeldaStatus.setAttribute('style', 'text-align : center');
+      if(x.Available == 'True') {
+        var TxtStatus = document.createTextNode('Available');
+        Fila.classList = 'RowGreen';
+      }else {
+        var TxtStatus = document.createTextNode('Loaned');
+        Fila.classList = 'RowRed';
+      }
+      var CeldaTaker = document.createElement('td');
+      CeldaTaker.setAttribute('style', 'text-align : center');
+      if(x.Loan == '') {
+        var TxtTaker = document.createTextNode('--');
+      }else {
+        var TxtTaker = document.createTextNode(x.Loan);
+      }
+      CeldaName.appendChild(TxtCelda);
+      Fila.appendChild(CeldaName);
+      CeldaStatus.appendChild(TxtStatus);
+      Fila.appendChild(CeldaStatus);
+      CeldaTaker.appendChild(TxtTaker);
+      Fila.appendChild(CeldaTaker);
+      TblBody.appendChild(Fila);
+      Cont++;
+  });
+  Tabla.appendChild(TblHead);
+  Tabla.appendChild(TblBody);
+}
